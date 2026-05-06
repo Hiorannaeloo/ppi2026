61b7bdcbb71783730ebc662dd7b1ce66784bac7d
Hiorannaeloo <eloa.siqueira@escolar.ifrn.edu.br>
2026-05-06 19:19:59 +0000

    aula 06/05 - introdução a javascript

---
+++ ppi26.js
@@ -0,0 +1,34 @@
+// Programação para Internet - 2026.1
+// Aluno: Hioranna Eloá Siqueira de Oliveira
+
+// Introdução à JavaScript
+
+console.log("Bem vindo a PPI!");
+
+// variáveis
+let name = "Hioranna";
+let age = 20;
+let isStudent = true; //A primeira sendo minuscula
+
+const PI = 3.14; // não pode ser reatribuída
+console.log("Valor de PI:", PI);
+
+// Operadores Aritméticos
+let x = 5 + 5;
+let y = "5" + 5; // concatenação = colar/juntar
+let z = "Hello" + 5;
+
+console.log(x**2, y, z);
+console.log(typeof x);
+
+//Ativar formatador prettier
+// ALT + SHIFT + F
+
+//Operadores relacionais
+console.log('5' != 5); // Diferente (comparação de valor)  
+console.log('5' == 5); // true (comparação de valor)
+console.log('5' === 5); // false (comparação de valor e tipo)
+
+// Operadores de incremento
+console.log(x++); // 10 (pós-incremento: retorna o valor antes de incrementar)
+console.log(x);
